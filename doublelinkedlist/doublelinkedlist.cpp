3c464926d2a65dd3e1796740988000d7286d4265
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-05-25 10:45:51 +0700

    Menambahkan Node baru

--- doublelinkedlist/doublelinkedlist.cpp
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -36,4 +36,7 @@ void Doublelinkedlist::addNote() {
 	cout << "\nEnter the roll number  of the student: ";
 	cin >> nim;
 	cout << "\nEnter the name of the student: ";
+	cin.ignore();
+	getline(cin, nm);
+	Node* newNode = new Node();
 }
