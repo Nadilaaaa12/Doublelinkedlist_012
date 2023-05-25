5c3e585b72e3a3e9e5808ac2aaf4f080279e3b0a
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-05-25 12:29:00 +0700

    fix

--- doublelinkedlist/doublelinkedlist.cpp
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -43,7 +43,7 @@ void Doublelinkedlist::addNote() {
 	newNode->name = nm;
 
 	if (START == NULL || nim <= START->noMhs) {
-		if (START |= NULL && nim == START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
 			cout << "\nDuplicate number not allowed" << endl;
 			return;
 		}
@@ -56,22 +56,22 @@ void Doublelinkedlist::addNote() {
 	}
 
 	Node* current = START;
-	while (current->next != NULL && nim > current->next->noMhs) {
+	while (current->next != NULL && nim > current->next->noMhs) 
 		current = current->next;
 
 		if (current->next != NULL && nim == current->next->noMhs) {
 			cout << "\nDuplicate roll numbers not allowed" << endl;
 			return;
-	}
+		}
 
 		newNode->next = current->next;
 		newNode->prev = current;
 		if (current->next != NULL)
 			current->next->prev = newNode;
 		current->next = newNode;
+	}
 
-
-		bool Doublelinkedlist::search(int rollNo, Node * *previous, Node * *current) {
+		bool Doublelinkedlist::search(int rollNo, Node** previous, Node** current) {
 			*previous = *current = START;
 			while (*current != NULL && rollNo != (*current)->noMhs) {
 				*previous = *current;
@@ -81,7 +81,131 @@ void Doublelinkedlist::addNote() {
 		}
 
 		bool Doublelinkedlist::deleteNode(int rollNo) {
+			Node* previous, * current;
+			previous = current = NULL;
+			if (search(rollNo, &previous, &current) == false)
+				return false;
 
+			if (current->next != NULL)
+				current->next->prev = previous;
+			if (previous != NULL)
+				previous->next = current->next;
+			else
+				START = current->next;
+
+			delete current;
+			return true;
 		}
-	}
-}
+
+		bool Doublelinkedlist::listEmpty() {
+			return (START == NULL);
+		}
+
+		void Doublelinkedlist::ascending() {
+			if (listEmpty())
+				cout << "\nList is empty" << endl;
+			else {
+				cout << "\nRecords in ascending order of roll number are:" << endl;
+				Node* currentNode = START;
+				while (currentNode != NULL) {
+					cout << currentNode->noMhs << " " << endl;
+					currentNode = currentNode->next;
+				}
+			}
+		}
+
+		void Doublelinkedlist::descending() {
+			if (listEmpty())
+				cout << "\nList is empty" << endl;
+			else {
+				cout << "\nRecords in descending order roll number are:" << endl;
+				Node* currentNode = START;
+				while (currentNode->next != NULL)
+					currentNode = currentNode->next;
+
+				while (currentNode != NULL) {
+					cout << currentNode->noMhs << " " << currentNode->name << endl;
+					currentNode = currentNode->prev;
+	        	}
+			}
+		}
+
+		void Doublelinkedlist::hapus() {
+			if (listEmpty()) {
+				cout << "\nList is empty" << endl;
+
+			}
+			cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+			int rollNo;
+			cin >> rollNo;
+			cout << endl;
+			if (Doublelinkedlist::deleteNode(rollNo) == false)
+				cout << "Record not found" << endl;
+			else
+				cout << "Record with roll number " << rollNo << " deleted" << endl;
+		}
+		void Doublelinkedlist::searchData() {
+			if (listEmpty() == true) {
+				cout << "\nList is empty" << endl;
+
+			}
+			Node* prev, * curr;
+			prev = curr = NULL;
+			cout << "\nEnter the roll number of the student whose record you want to search: ";
+			int num;
+			cin >> num;
+			if (Doublelinkedlist::search(num, &prev, &curr) == false)
+				cout << "\nRecord not found" << endl;
+			else {
+				cout << "\nRecord found" << endl;
+				cout << "\nRoll number: " << curr->noMhs << endl;
+				cout << "\nName: " << curr->name << endl;
+			}
+		}
+
+		int main() {
+			Doublelinkedlist obj;
+			while (true) {
+				try {
+					cout << "\nMenu" << endl;
+					cout << "1. Add a record to the list" << endl;
+					cout << "2. Delete a record from the list" << endl;
+					cout << "3. View all records in the ascending order of roll numbers" << endl;
+					cout << "4. View all records in the descending order of roll numbers" << endl;
+					cout << "5. Search for a record in the list" << endl;
+					cout << "6. Exit" << endl;
+					cout << "\nEnter your choice (1-6): ";
+					char ch;
+					cin >> ch;
+
+					switch (ch) {
+					case '1':
+						obj.addNote();
+						break;
+					case '2':
+						obj.hapus();
+						break;
+					case '3':
+						obj.hapus();
+						break;
+					case '4':
+						obj.ascending();
+						break;
+					case '5':
+						obj.searchData();
+						break;
+					case '6':
+						return 0;
+					default:
+						cout << "\nInvalid option" << endl;
+						break;
+					}
+				}
+				catch (exception& e) {
+					cout << "Check for the values entered." << endl;
+				}
+			}
+		}
+	
+	
+
