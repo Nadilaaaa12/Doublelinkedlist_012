f437c7d0f858a1a67384cc902288acf8db747c7f
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-05-25 11:05:23 +0700

    pointer menunjuk ke simpul dalam linkedlist

--- doublelinkedlist/doublelinkedlist.cpp
+++ doublelinkedlist/doublelinkedlist.cpp
@@ -57,6 +57,10 @@ void Doublelinkedlist::addNote() {
 
 	Node* current = START;
 	while (current->next != NULL && nim > current->next->noMhs) {
+		current = current->next;
 
+		if (current->next != NULL && nim == current->next->noMhs) {
+			cout << "\nDuplicate roll numbers not allowed" << endl;
+	}
 	}
 }
